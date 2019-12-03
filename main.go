33c9652c93f965bb2d3eb23c4bb9e1f7c077ba1c
Taylor Lea <taylorl@jfrog.com>
2019-12-03 13:13:25 -0800

    Implement go http server and checkauth endpoint

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const apiKey = "secretKey"
+
+type CheckAuthResponse struct {
+	Valid bool
+	Error string
+}
 
 func main() {
-	fmt.Println("hello go")
+	// Routes
+	http.HandleFunc("/api/checkauth", checkAuth)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Something went wrong: ", err)
+		return
+	}
+}
+
+// The Test URL you can use to test your API key with the provider using the "Test" button
+func checkAuth(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("checkAuth")
+	resp := CheckAuthResponse{true, ""}
+	key := r.Header.Get("apiKey")
+	if key != apiKey {
+		fmt.Println("not working", key)
+		resp = CheckAuthResponse{Valid: false, Error: "Invalid Api Key"}
+	}
+	js, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
