c1c4832bc86228b03b3c97d5bb4ae458fc359961
Taylor Lea <taylorl@jfrog.com>
2019-12-03 16:30:45 -0800

    Server works with a fake db
    The db only includes python requests

--- main.go
+++ main.go
@@ -51,9 +51,9 @@ type ComponentInfoResponse struct {
 }
 
 func main() {
-	// Routes
-	http.HandleFunc("/api/checkauth", checkAuth)
-	http.HandleFunc("/api/componentinfo", componentInfo)
+	// Routes: must be supplied to x-ray during integration setup as TestURL and URL
+	http.HandleFunc("/api/checkauth", checkAuth) // TestURL
+	http.HandleFunc("/api/componentinfo", componentInfo) // URL
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -79,15 +79,57 @@ func checkAuth(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
+// This endpoint provides information to XRay about components
 func componentInfo(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
 	requestPayload := ComponentInfoRequest{}
 	err = json.Unmarshal(body, &requestPayload)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
+	// Get all the components from the "db".
+	// The db is just a json file with fake data about components.
+	db := getDB()
+
+	// Get matching components from db
+	responsePayload := findComponents(requestPayload.Components, db)
+
+	js, err := json.Marshal(responsePayload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+// Read json file "database" into memory
+func getDB() []ComponentInfo {
+	file, _ := ioutil.ReadFile("db.json")
+	var data []ComponentInfo
+	_ = json.Unmarshal(file, &data)
+	return data
+}
+
+// Search db for matching components and return
+func findComponents(components []Component, db []ComponentInfo) ComponentInfoResponse {
+	matches := ComponentInfoResponse{}
+	// Check database for matching components
+	for _, component := range components {
+		result := ComponentInfo{}
+		for _, item := range db {
+			if item.ComponentID == component.ComponentID {
+				result = item
+				result.Provider = providerName
+				break
+			}
+		}
+		matches.Components = append(matches.Components, result)
+	}
+	return matches
 }
